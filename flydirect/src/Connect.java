d82a21669d5971f29de5def18356c21200109f48
AonachMor <91143658+AonachMor@users.noreply.github.com>
2026-05-13 14:36:53 +0100

    Update Connect.java

--- flydirect/src/Connect.java
+++ flydirect/src/Connect.java
@@ -10,8 +10,8 @@ import java.sql.*;
 public class Connect
 {
 	Connection conn = null;	
-	String username = "charles";
-	String password = "atlas";
+	String username = "xxxxxx";
+	String password = "xxxxxx";
 	String defaultHost = "jdbc:mysql://localhost/FlyAtlasDB?useUnicode=true&characterEncoding=UTF-8";
 	
 	ResultSet resSet;
